f0df086695fcc31d161d079c0af82d210c05d083
Диана Пынзарь <dipynzar@MacBook-Pro-Diana.local>
2020-09-17 10:31:39 +0400

    Updates

---
+++ precache-manifest.8cc3208d645e0038506c5b5cd061feab.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ac48e794abf793f3eccadda71bbc7d9",
+    "url": "/TestFunbox/index.html"
+  },
+  {
+    "revision": "a2267f00eaf79707cef8",
+    "url": "/TestFunbox/static/css/main.f34d01eb.chunk.css"
+  },
+  {
+    "revision": "6e79ea34cccacad2fa3c",
+    "url": "/TestFunbox/static/js/2.d5f0462d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TestFunbox/static/js/2.d5f0462d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2267f00eaf79707cef8",
+    "url": "/TestFunbox/static/js/main.07dff4f5.chunk.js"
+  },
+  {
+    "revision": "cd4ad076474dcb58e858",
+    "url": "/TestFunbox/static/js/runtime-main.a9c57702.js"
+  },
+  {
+    "revision": "41729db3b29d98b9fa0bedbe0f91a1e7",
+    "url": "/TestFunbox/static/media/Exo2.0-Thin.41729db3.woff"
+  },
+  {
+    "revision": "128b18468ecb64ed43af0c593b6b4f77",
+    "url": "/TestFunbox/static/media/Trebuchet-MS.128b1846.woff"
+  },
+  {
+    "revision": "453a322ece668191a80deb4e38075aac",
+    "url": "/TestFunbox/static/media/cat.453a322e.png"
+  },
+  {
+    "revision": "79d68011797f7124dd17f3a480a7a01f",
+    "url": "/TestFunbox/static/media/wrapperBackground.79d68011.png"
+  }
+]);
